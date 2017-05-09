c49d4fab01fb62a91d21ce7eac3177ade7aeeede
Matt Fehskens <matt.fehskens@gmail.com>
2017-05-08 20:33:54 -0400

    Added logging, compiled outputs info level logs

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,8 +1,12 @@
 defmodule Issues.GitHubIssues do
+
+  require Logger
+
   @github_url Application.get_env(:issues, :github_url)
   @user_agent [ { "User-agent", "Elixir matt.fehskens@gmail.com" } ]
 
   def fetch(user, project) do
+    Logger.info "Fetch user #{ user }'s project #{ project }"
     issues_url(user, project)
       |> HTTPoison.get(@user_agent)
       |> handle_response
@@ -13,10 +17,13 @@ defmodule Issues.GitHubIssues do
   end
 
   def handle_response({ :ok, %{ status_code: 200, body: body } }) do
+    Logger.info "Successful response"
+    Logger.debug fn -> inspect(body) end
     { :ok, Poison.Parser.parse!(body) }
   end
 
-  def handle_response({ _, %{ status_code: _, body: body } }) do
+  def handle_response({ _, %{ status_code: status, body: body } }) do
+    Logger.error "Error #{ status } returned"
     { :error, Poison.Parser.parse!(body) }
   end
 end
